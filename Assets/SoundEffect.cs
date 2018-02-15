a6c8b74ea094fcba599ddeb9c46c20ee21cae449
yuusukemorio <Morimori2003@gmail.com>
2018-02-15 15:35:42 +0900

    GoForwardUnityChanの課題です　よろしくお願いします

---
+++ Assets/SoundEffect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffect : MonoBehaviour {
+
+    private AudioSource Block;
+
+    // Use this for initialization
+    void Start()
+    {
+        //鳴らす音を指定
+        Block = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
+        if (tag == "Block") {
+            //Block.PlayOneShot(Block.clip);
+            Block.Play();
+        }
+    }
+}
